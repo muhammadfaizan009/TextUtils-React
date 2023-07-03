df18b6e898db24acfb8eabcd79f5ef0da9f63b4d
muhammadfaizan009 <muhammadfaizanmu75@gmail.com>
2023-07-03 21:21:57 +0500

    This is a TextUtils app

---
+++ src/components/TextForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to Uppercase","success");
+  };
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to Lowercase","success");
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <div className="container"
+      style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'black'}}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowClick}>
+          Convert to lowercase
+        </button>
+      </div>
+      <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something to preview here."}</p>
+      </div>
+    </div>
+  );
+}
